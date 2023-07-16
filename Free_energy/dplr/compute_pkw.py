b615b26cafadd23977e603e7e9599e13a6284fbb
Marcos Calegari <marcos.caa@gmail.com>
2023-07-16 01:42:16 -0400

    Creating repo with data, input files, graphs

---
+++ Free_energy/dplr/compute_pkw.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+c0=6.023E23/1.E27 #Standard concentration in atoms/A3
+
+def load_fes(sys):
+  f=np.loadtxt("{}/fes_.dat".format(sys)).T
+  return f
+
+def integrate(F,S,Sc):
+  intF=4.E0*np.pi*np.trapz(S[S<Sc]**2.E0*np.exp(-F[S<Sc]),S[S<Sc])*(S[-1]-S[-2])
+  return intF
+
+def compute_pkw(f,Ssep):
+  pHinv=c0*integrate(f[1],f[0],Ssep)
+  pH=np.log10(pHinv)
+  return 2*pH
+  
+def main():
+  sys=[64,128,256,512,1024]
+  pKw=np.zeros(len(sys))
+  l=4.0
+  for s in range(len(sys)):
+    fes=load_fes(sys[s])
+    fes[1]-=np.mean(fes[1][-5:])
+    pKw[s]=compute_pkw(fes,l) 
+
+def plot_curves():
+  plt.xlabel('Number of H$_2$O molecules',fontsize=9)
+  plt.ylabel('pKw',fontsize=9)
+  plt.legend()
+  plt.tight_layout()
+  plt.show()
+
+fig,ax=plt.subplots(1,1,figsize=(3.3,3.3))
+main()
+plot_curves()
---
+++ Free_energy/dplr/reweight.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sys import argv
+
+colvar=np.loadtxt('COLVAR').transpose()
+
+Kb=0.00831446 #kJ/mol
+T=330 #K
+L=float(argv[1])
+rho=0.0334
+
+nbins=int(np.max(colvar[1])/0.05)
+bias=colvar[3]+colvar[5]
+hist,bins=np.histogram(colvar[1],bins=nbins,weights=np.exp(bias/(Kb*T)))
+bins=(bins[1:]+bins[:-1])/2.
+hist/=bins**2*(bins[1]-bins[0])
+
+hist/=np.trapz(bins**2*hist,bins)*(bins[1]-bins[0])/rho
+
+fes=-np.log(hist)
+
+filt=bins<L/2.
+np.savetxt('fes.dat',np.vstack((bins[filt],fes[filt])).transpose())
+
